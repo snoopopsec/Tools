a7aacb2cfd0b50181b79d085ab4f27123d69c637
snoopopsec <92749864+snoopopsec@users.noreply.github.com>
2025-09-16 16:56:41 -0700

    Update and rename popcmd.cpp to popcmd.dll.cpp

--- popcmd/popcmd.dll.cpp
+++ popcmd/popcmd.dll.cpp
@@ -1,4 +1,4 @@
-// popcmd.cpp
+// popcmd.dll.cpp
 #include <windows.h>
 
 BOOL APIENTRY DllMain(HMODULE hModule,
@@ -6,7 +6,8 @@ BOOL APIENTRY DllMain(HMODULE hModule,
                       LPVOID lpReserved)
 {
     if (ul_reason_for_call == DLL_PROCESS_ATTACH) {
-        WinExec("cmd.exe", SW_SHOW);
+        WinExec("cmd.exe", SW_SHOW);  // Pops visible cmd window
     }
+
     return TRUE;
 }
